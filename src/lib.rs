876ece7cb223ebadcc8d72cd0d2a31576e623695
cndoit18 <cndoit18@outlook.com>
2024-11-30 01:59:12 +0800

    add subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
-use axum::{http::StatusCode, routing::get, serve::Serve, Router};
+use axum::{
+    extract::Form,
+    http::StatusCode,
+    routing::{get, post},
+    serve::Serve,
+    Router,
+};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Serve<Router, Router>, std::io::Error> {
-    let app: Router = Router::new().route("/health_check", get(health_check));
+    let app: Router = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
     Ok(axum::serve(listener, app))
 }
 
 async fn health_check() -> StatusCode {
     StatusCode::OK
+}
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(Form(form_data): Form<FormData>) -> Result<String, StatusCode> {
+    Ok(format!("Welcome {}({})!", form_data.name, form_data.email))
 }
