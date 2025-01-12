182915bb685439d4cd096402dd8a9281789291bd
cndoit18 <cndoit18@outlook.com>
2025-01-13 00:54:18 +0800

    send email
    Change-Id: I7da34033510b65c23b7d7aad2a67cbadfe56d1c3

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,11 +1,60 @@
 use secrecy::ExposeSecret;
 use secrecy::SecretString;
 use serde::Deserialize;
+use url::Url;
+use validator::ValidateEmail;
+
+use crate::domain::SubscriberEmail;
 
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
+}
+
+#[derive(Deserialize)]
+pub struct EmailClientSettings {
+    pub sender_email: String,
+    pub email_service: EmailService,
+}
+
+#[derive(Deserialize)]
+pub enum EmailService {
+    #[serde(rename = "smtp")]
+    Smtp(SmtpSettings),
+}
+
+#[derive(Deserialize)]
+pub struct SmtpSettings {
+    pub host: String,
+    pub port: u32,
+    pub username: SecretString,
+    pub password: SecretString,
+}
+
+impl EmailClientSettings {
+    pub fn connection_string(&self) -> SecretString {
+        match &self.email_service {
+            EmailService::Smtp(smtp_settings) => {
+                SecretString::from(Url::parse(&format! {
+                    "smtp://{}:{}@{}:{}",  smtp_settings.username.expose_secret(), smtp_settings.password.expose_secret(),smtp_settings.host, smtp_settings.port,
+                }).unwrap().as_str())
+            }
+        }
+    }
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        {
+            let s = self.sender_email.clone();
+            if ValidateEmail::validate_email(&s) {
+                return SubscriberEmail::parse(s);
+            }
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
 }
 
 #[derive(Deserialize)]
---
+++ src/email_client.rs
@@ -0,0 +1,92 @@
+use crate::domain::SubscriberEmail;
+use lettre::message::{header, MultiPart, SinglePart};
+use lettre::{Message, SmtpTransport, Transport};
+
+#[allow(dead_code)]
+pub struct EmailClient {
+    mailer: SmtpTransport,
+    sender: SubscriberEmail,
+}
+
+impl EmailClient {
+    pub fn new(smtp_url: &str, sender: SubscriberEmail) -> Self {
+        Self {
+            mailer: SmtpTransport::from_url(smtp_url).unwrap().build(),
+            sender,
+        }
+    }
+
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        let email = Message::builder()
+            .from(self.sender.as_ref().parse().unwrap())
+            .to(recipient.as_ref().parse().unwrap())
+            .subject(subject)
+            .multipart(
+                MultiPart::alternative() // This is composed of two parts.
+                    .singlepart(
+                        SinglePart::builder()
+                            .header(header::ContentType::TEXT_PLAIN)
+                            .body(text_content.to_string()), // Every message should have a plain text fallback.
+                    )
+                    .singlepart(
+                        SinglePart::builder()
+                            .header(header::ContentType::TEXT_HTML)
+                            .body(html_content.to_string()),
+                    ),
+            )
+            .map_err(|_| "failed to build email")?;
+        self.mailer
+            .send(&email)
+            .map_err(|err| format!("failed to send email: {}", err))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::faker::internet::raw::{DomainSuffix, Password, SafeEmail};
+    use fake::faker::lorem::raw::{Paragraph, Sentence};
+    use fake::locales::EN;
+    use fake::Fake;
+    use maik::MockServer;
+    use url::Url;
+
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let sender_email = SubscriberEmail::parse(SafeEmail(EN).fake::<String>()).unwrap();
+        let sender_password = Password(EN, 8..15).fake::<String>();
+
+        let mut mock_server = MockServer::new(DomainSuffix(EN).fake::<String>().as_str());
+        mock_server.add_mailbox(sender_email.as_ref(), sender_password.as_ref());
+        mock_server.start();
+        let email_client = EmailClient::new(
+            Url::parse(&format!(
+                "smtp://{}:{}@{}:{}",
+                sender_email.as_ref(),
+                sender_password.as_str(),
+                mock_server.host(),
+                mock_server.port()
+            ))
+            .unwrap()
+            .as_str(),
+            sender_email,
+        );
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail(EN).fake()).unwrap();
+        let subject = Sentence(EN, 1..2).fake::<String>();
+        let content = Paragraph(EN, 1..10).fake::<String>();
+        email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await
+            .unwrap();
+    }
+}
